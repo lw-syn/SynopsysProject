8306a678d9c426187e100a22b74d123a06634375
lw-sys <lawrence.wong.cw@gmail.com>
2016-02-21 16:11:35 -0700

    Fix the classpath of unit test

--- src/main/java/Calculation.java
+++ src/main/java/Calculation.java
@@ -1,4 +1,4 @@
-/* Calculation Class
+/** Calculation Class
  * It performs the arithmetic operator of addition, subtraction, multiplication, division
  * 
  * @author Lawrence Wong
@@ -10,35 +10,52 @@ public class Calculation {
 	private int _first;
 	private int _second;
 
-	// Default constructor
+	/**
+	 * Default constructor to set both _frist and _second to zero
+	 */
 	Calculation (){
 		_first = 0;
 		_second = 0;
 	}
 	
-	// Overloaded constructor with two parameters
+	/**
+	 * Overloaded constructor with two parameters to set _frist and _second to pass in parameters
+	 * @param first
+	 * @param second
+	 */
     Calculation(int first, int second){
     	_first = first;
     	_second = second;
     }
     
-    // Perform Addition
+    /**
+     * Perform Addition
+     * @return int
+     */
     public int add(){
     	return(_first + _second);
     }
 
-    // Perform Subtraction
+    /**
+     * Perform Subtraction
+     * @return int
+     */
     public int sub(){
     	return(_first - _second);
     }
 
-    // Perform Multiplication
+    /**
+     *  Perform Multiplication
+     * @return int
+     */
     public int mult(){
     	return(_first * _second);
     }
 
-    // Perform Division
-    // If the denominator is 0, it return Double.NaN
+    /**
+     *  Perform Division. If the denominator is 0, it return Double.NaN
+     * @return
+     */
     public double div(){
     	if (_second != 0){
     		return(_first / _second);
